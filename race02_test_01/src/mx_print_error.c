93413e000054ad3bfd165e3a331fe374fe52ba01
Bogdan Melnik <113630039+WiZe1007@users.noreply.github.com>
2023-02-17 18:16:28 +0200

    Delete race02_test_01 directory

--- race02_test_01/src/mx_print_error.c
+++
@@ -1,9 +0,0 @@
-#include "way_home.h"
-#include <stdlib.h>
-
-void mx_print_error(const char *message) {
-    mx_printerr("Error: ");
-    mx_printerr(message);
-    mx_printerr("\n");
-    exit(1);
-}
